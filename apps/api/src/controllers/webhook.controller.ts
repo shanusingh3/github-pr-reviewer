ea4cb655a56945a64053eec044ad19d5570efc9e
Shanu Singh <iamshanusingh@gmail.com>
2026-05-31 14:40:40 +0530

    console added in webhook

--- apps/api/src/controllers/webhook.controller.ts
+++ apps/api/src/controllers/webhook.controller.ts
@@ -1,7 +1,15 @@
-import {Request, Response} from 'express';
+import { Request, Response } from 'express';
 
 
 export const githubWebook = (req: Request, res: Response) => {
+
+    console.log({
+        action: req.body.action,
+        repository: req.body.repository?.name,
+        prNumber: req.body.pull_request?.number,
+        title: req.body.pull_request?.title,
+    });
+
     res.status(200).json({
         success: true,
         message: 'Webhook received'
