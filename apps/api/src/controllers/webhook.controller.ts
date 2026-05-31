ae5dc4f2e1b9783c9044259b3b34d371c209b7b2
Shanu Singh <iamshanusingh@gmail.com>
2026-05-31 14:41:19 +0530

    pr-01-auto

--- apps/api/src/controllers/webhook.controller.ts
+++ apps/api/src/controllers/webhook.controller.ts
@@ -8,6 +8,7 @@ export const githubWebook = (req: Request, res: Response) => {
         repository: req.body.repository?.name,
         prNumber: req.body.pull_request?.number,
         title: req.body.pull_request?.title,
+        
     });
 
     res.status(200).json({
